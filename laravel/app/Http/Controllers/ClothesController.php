5cb714cb4fe3e7528b82bf8353ba472e2f230f31
Marco Tuzza <marcotuzza@hotmail.it>
2020-07-06 18:58:43 +0200

    Errore cuold not find driver

--- laravel/app/Http/Controllers/ClothesController.php
+++ laravel/app/Http/Controllers/ClothesController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Clothes;
 
 class ClothesController extends Controller
 {
     public function index() {
-        return view('clothes');
+        $all_clothes = Clothes::all();
+        dd($all_clothes);
     }
 };
